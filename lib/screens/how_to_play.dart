d9e912fdfaba4f8882c3d6533d3cf9888cacc6bf
LittleCanOpener <hrafn8@gmail.com>
2023-11-29 16:04:54 +0000

    adding a body
    for how to play

--- lib/screens/how_to_play.dart
+++ lib/screens/how_to_play.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class HowToPlay extends StatelessWidget {
   const HowToPlay({super.key});
+
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -19,6 +21,21 @@ class HowToPlay extends StatelessWidget {
             child: const Text('How To Play'),
           ),
           const SizedBox(width: 50),
+        ],
+      ),
+      // Body for "How to play"
+      body: const Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                 Center(
+                  child: Text('PlaceHolder',),
+                 ),
+              ],
+            ),
+          ),
         ],
       ),
     );
